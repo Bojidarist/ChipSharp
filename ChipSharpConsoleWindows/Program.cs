d5918e37f92bf6a0bf6bdcec0513018fe5f3a19b
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-09-17 18:04:02 +0300

    Add ChipSharpConsoleWindows project

---
+++ ChipSharpConsoleWindows/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChipSharpConsoleWindows
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
